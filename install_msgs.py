9a1e9219e45be27eea34a787fd53fb05e4e558f7
Tamaki Nishino <otamachan@gmail.com>
2016-03-26 01:16:30 +0900

    Fix typo

--- install_msgs.py
+++ install_msgs.py
@@ -11,6 +11,6 @@ MESSAGES = [
     # already insatlled under site-packages.
     ('ros/std_msgs', '0.5.10', '', []),
     ('ros/ros_comm', '1.12.0', 'clients/roscpp', []),
-    ('ros/ros_comm_msgs', '1.11.2', 'rosgraph_msgs', ['std_msg']),
+    ('ros/ros_comm_msgs', '1.11.2', 'rosgraph_msgs', ['std_msgs']),
 ]
 get_rospy.install_messages(MESSAGES)
